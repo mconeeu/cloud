eb29c8dc387cfaaddf24acecf418ae9b2d94ef78
Dominik Lippl <twinsterhd@mcone.eu>
2018-04-29 16:01:49 +0200

    Fix Push

--- wrapper/src/main/java/eu/mcone/cloud/wrapper/WrapperServer.java
+++ wrapper/src/main/java/eu/mcone/cloud/wrapper/WrapperServer.java
@@ -113,8 +113,7 @@ public class WrapperServer {
         }
 
         Logger.log("Enable progress", "Trying to connect to master...");
-        //nettyBootstrap = new ClientBootstrap(config.getConfig().getString("master-hostname"), config.getConfig().getInt("master-port"));
-        nettyBootstrap = new ClientBootstrap("213.202.245.107", 4567);
+        nettyBootstrap = new ClientBootstrap(config.getConfig().getString("master-hostname"), config.getConfig().getInt("master-port"));
 
         Logger.log("Enable progress", ConsoleColor.GREEN+"Enable process finished! CloudWrapper seems to be ready! Waiting for connections...\n");
     }
