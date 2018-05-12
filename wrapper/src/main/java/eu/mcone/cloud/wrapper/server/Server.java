d04bab867e21c9ce40d6e9482764612e232c68a4
Dominik Lippl <twinsterhd@mcone.eu>
2018-05-12 09:34:46 +0200

    Implements Debug function in class Server

--- wrapper/src/main/java/eu/mcone/cloud/wrapper/server/Server.java
+++ wrapper/src/main/java/eu/mcone/cloud/wrapper/server/Server.java
@@ -109,7 +109,16 @@ public abstract class Server {
                         CloudWorld world = new WorldDownloader(w).downloadWorld();
                         worldArray.add(gson.toJsonTree(world, CloudWorld.class));
 
-                        Logger.log(getClass(), "["+info.getName()+"] Implementing World "+w);
+                        Logger.log(getClass(), "["+info.getName()+"] Implementing World " + w);
+
+                        System.out.println("World-Path: " + world.getFilePath());
+                        System.out.println("World-Difficulty: " + world.getDifficulty());
+                        System.out.println("World-Environmen: " + world.getEnvironment());
+                        System.out.println("World-Generator: " + world.getGenerator());
+                        System.out.println("World-Name: " + world.getName());
+                        System.out.println("World-Properties: " + world.getProperties());
+                        System.out.println("World-SpawnLocation: " + world.getSpawnLocation());
+                        System.out.println("World-WorldType: " + world.getWorldType());
                         new UnZip(
                                 world.getFilePath(),
                                 serverDir.getPath() + File.separator + w
